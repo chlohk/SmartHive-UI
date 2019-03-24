fe38b84450c04eab4588c9ad9baea5ac57eed18c
jaanikaraik <jaraik@ttu.ee>
2019-03-24 19:56:52 +0200

    attempted to add a more advanced test

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsComponent } from './settings.component';
+import {Settings} from "http2";
+import {By} from "@angular/platform-browser";
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -21,5 +23,18 @@ describe('SettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should let create new colony', () => {
+    let fixture = TestBed.createComponent(SettingsComponent);
+    let testComponent = fixture.debugElement.componentInstance;
+    let newColonyButton = fixture.debugElement.query(By.css('newColonyButton'));
+
+    fixture.detectChanges();
+    newColonyButton.nativeElement.click();
+  });
+
+  it('should let click on work part', () => {
+
   });
 });
