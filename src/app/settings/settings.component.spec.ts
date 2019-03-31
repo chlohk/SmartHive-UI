7683fcc5f7f5543a7fa3255d583d6b3bdafb6960
jaanikaraik <jaraik@ttu.ee>
2019-03-31 12:19:59 +0300

    tests improved

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,37 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
 import { SettingsComponent } from './settings.component';
-import {Settings} from "http2";
-import {By} from "@angular/platform-browser";
+import {NewColonyComponent} from "./new-colony/new-colony.component";
 
 describe('SettingsComponent', () => {
-  let component: SettingsComponent;
-  let fixture: ComponentFixture<SettingsComponent>;
-
+  function setup(){
+    const fixture = TestBed.createComponent(SettingsComponent);
+    const component = fixture.componentInstance;
+    return { fixture, component }
+  }
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
+      declarations: [ SettingsComponent, NewColonyComponent ]
     })
-      .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SettingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should let create new colony', () => {
-    let fixture = TestBed.createComponent(SettingsComponent);
-    let testComponent = fixture.debugElement.componentInstance;
-    let newColonyButton = fixture.debugElement.query(By.css('newColonyButton'));
-
-    fixture.detectChanges();
-    newColonyButton.nativeElement.click();
+  it('#newColonyButton pressed then #appNewColonyComponent is visible', () => {
+    const {fixture, component} = setup();
+    const compile = fixture.debugElement.nativeElement;
+    const newColonyButton = compile.querySelector('#newColonyButton');
+    const appNewColonyComponent = compile.querySelector('#appNewColonyComponent ');
+    expect(appNewColonyComponent).toBeTruthy();
   });
 
   it('should let click on work part', () => {
